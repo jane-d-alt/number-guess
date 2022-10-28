7f17508e19f458e1eb04452ee942691c5dc9195f
jane-d-alt <51848939+jane-d-alt@users.noreply.github.com>
2022-10-28 15:47:46 +0100

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -4,16 +4,13 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 N=$((1 + $RANDOM % 1000))
 NUM='^[0-9]+$'
 GUESSES=0
-echo $N
-USERNAME_PROMPT="Enter your username:"
 
-echo $USERNAME_PROMPT
+echo "Enter your username:"
 read USERNAME
 
 PREV_GAMES=$($PSQL "SELECT COUNT(username) FROM games WHERE username='$USERNAME'")
-echo $PREV_GAMES
 
-if [[ $PREV_GAMES == 0 ]]
+if [[ $PREV_GAMES -eq 0 ]]
 then
  echo -e "Welcome, $USERNAME! It looks like this is your first time here."
 else
@@ -24,7 +21,7 @@ fi
 TAKE_GUESS() {
   if [[ $1 ]]
   then
-    echo -e "\n$1"
+    echo -e "$1"
   fi
   read GUESS
 if [[ $GUESS =~ $NUM ]]
@@ -32,7 +29,7 @@ then
  GUESSES=$((GUESSES+1))
  if [[ $GUESS -eq $N ]]
  then
-  echo -e "You guessed it in $GUESSES tries. The secret number was $N. Nice job!"
+  echo "You guessed it in $GUESSES tries. The secret number was $N. Nice job!"
   SAVE_GAME=$($PSQL "INSERT INTO games(username, guesses) VALUES('$USERNAME', $GUESSES)")
  elif [[ $GUESS -lt $N ]]
  then
@@ -41,9 +38,8 @@ then
   TAKE_GUESS "It's lower than that, guess again:"
  fi
 else
- TAKE_GUESS "Your guess was '$GUESS'. That is not an integer, guess again:"
+ TAKE_GUESS "That is not an integer, guess again:"
 fi
 }
 
 TAKE_GUESS "Guess the secret number between 1 and 1000:"
-
